6c534ec9c2579c256c900609be3e2118a4923ef8
scrawl <scrawl@email.com>
2021-06-24 00:15:19 +0100

    [~] minor updates k.c,p.c

--- src/k.c
+++ src/k.c
@@ -1,13 +1,13 @@
 /* main file
- * currently runs tests on latest interpreter modules.
- * below tests scanner in p.c (parser to be added) 
+ * currently runs tests on latest interpreter modules
+ * below tests scanner in p.c (parser to be added)
  */
 
 #include "a.h"
 #include "p.h"
 
-//<! global source string
+// global source string
 S a="123  +4.*xyz- 5.67;\"abc\",\"def\";  ";
 
-//<!tests
-I main(){O("str: %s\n\n",a);Ti(a);T t;W(END-(t=nt(a)).t,pt(t));pt(t);R 0;}
+// tests
+I main(){O("source: %s\ntokens:\n",a);Ti(a);T t;W(END-(t=nt(a)).t,pt(t));pt(t);R 0;}
