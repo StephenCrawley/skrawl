ef3658dc41857042d1f3e615b3c2906621ce045a
scrawl <scrawl@email.com>
2021-08-03 01:43:23 +0100

    [~] (expr)-NUM fixed

--- src/p.h
+++ src/p.h
@@ -1,13 +1,13 @@
 #ifndef PARSE
 #define PARSE
 #include "a.h"
-#define DBGT 1 //<! debug token
+#define DBGT 0 //<! debug token
 #define DBGP 0 //<! debug parse
 
 I vt[26]; //<! value table (TODO: move to vm module)
 
 /*            (  )  {  }  [  ]  ;  :  =  <  >  |  ?  +  -  *  %  !  "  @  ~  #  ,  .  '  /  \  1   "a" foo \0  not recognised */
-typedef enum {LP,RP,LB,RB,LS,RS,SC,CL,EQ,LA,RA,PI,QM,PL,HI,ST,DV,BA,QT,AT,TL,HS,CM,DT,AP,FS,BS,NUM,STR,ID, END,NR}TT; 
+typedef enum {LP,RP,LB,RB,LS,RS,SC,CL,EQ,LA,RA,PI,QM,PL,HY,ST,DV,BA,QT,AT,TL,HS,CM,DT,AP,FS,BS,NUM,STR,ID, END,NR}TT; 
 typedef struct {S s;I l;TT t;}T; //<! token. start,length,token type
 typedef struct {S s,c;T b[128];T *bp;}TS; //<! token scanner. start,current,buffer,buffer pointer
 TS ts;
