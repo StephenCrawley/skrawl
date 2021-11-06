002d8915e59a4a8edd85be8c7efb2f4716e7a88c
scrawl <scrawl@email.com>
2021-11-06 04:54:47 +0000

    [~] fixed join for floats

--- src/v.c
+++ src/v.c
@@ -13,9 +13,9 @@ K sum(K x,K y){DYAD_INIT(KF);DYAD_EXEC(+);R z;}
 K sub(K x,K y){DYAD_INIT(KF);DYAD_EXEC(-);R z;}
 K prd(K x,K y){DYAD_INIT(KF);DYAD_EXEC(*);R z;}
 K eq(K x,K y){DYAD_INIT(KJ);DYAD_EXEC_ZA(==,xJ);R z;}
-K lt(K x,K y){DYAD_INIT(KJ);DYAD_EXEC_ZA(<, xJ); R z;}
-K gt(K x,K y){DYAD_INIT(KJ);DYAD_EXEC_ZA(>, xJ); R z;}
-K cat(K x,K y){P(ABS(xt)-ABS(yt),E_TYP);K z=k(KJ,xn+yn);J j=0;
+K lt(K x,K y){DYAD_INIT(KJ);DYAD_EXEC_ZA(<, xJ);R z;}
+K gt(K x,K y){DYAD_INIT(KJ);DYAD_EXEC_ZA(>, xJ);R z;}
+K cat(K x,K y){P(ABS(xt)!=ABS(yt),E_TYP);K z=k(ABS(xt),xn+yn);J j=0;
  if(KJ==ABS(xt)){DO(xn,xJ(z)[j]=xJ(x)[i];++j);DO(yn,xJ(z)[j]=xJ(y)[i];++j)}else{DO(xn,xF(z)[j]=xF(x)[i];++j);DO(yn,xF(z)[j]=xF(y)[i];++j)};
  r0(x);r0(y);R z;}
 // % is special. always returns float so one arg must be float
