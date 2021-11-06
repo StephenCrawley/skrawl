d5c20c96e8caf70dba1b336bff81f43f2db43c27
scrawl <scrawl@email.com>
2021-11-06 04:28:55 +0000

    [~] refactored v.h macros

--- src/v.h
+++ src/v.h
@@ -18,29 +18,44 @@ K bng(K x,K y);
  zt=MAX(maxt,zt);                  \
  K z=k(zt,zn);
 
-#define DYAD_EXEC(op)  \
- /* TODO : refactor below */\
- /*a op a*/ \
- if     (-KJ==xt && -KJ==yt){xJ(z)[0]=xJ(x)[0] op xJ(y)[0];} \
- else if(-KJ==xt && -KF==yt){xF(z)[0]=xJ(x)[0] op xF(y)[0];} \
- else if(-KF==xt && -KJ==yt){xF(z)[0]=xF(x)[0] op xJ(y)[0];} \
- else if(-KF==xt && -KF==yt){xF(z)[0]=xF(x)[0] op xF(y)[0];} \
- /*a op v*/ \
- else if(-KJ==xt && KJ==yt){DO(zn,xJ(z)[i]=xJ(x)[0] op xJ(y)[i]);} \
- else if(-KJ==xt && KF==yt){DO(zn,xF(z)[i]=xJ(x)[0] op xF(y)[i]);} \
- else if(-KF==xt && KJ==yt){DO(zn,xF(z)[i]=xF(x)[0] op xJ(y)[i]);} \
- else if(-KF==xt && KF==yt){DO(zn,xF(z)[i]=xF(x)[0] op xF(y)[i]);} \
- /*v op a*/ \
- else if(KJ==xt && -KJ==yt){DO(zn,xJ(z)[i]=xJ(x)[i] op xJ(y)[0]);} \
- else if(KJ==xt && -KF==yt){DO(zn,xF(z)[i]=xJ(x)[i] op xF(y)[0]);} \
- else if(KF==xt && -KJ==yt){DO(zn,xF(z)[i]=xF(x)[i] op xJ(y)[0]);} \
- else if(KF==xt && -KF==yt){DO(zn,xF(z)[i]=xF(x)[i] op xF(y)[0]);} \
- /*v op v*/ \
- else if(KJ==xt && KJ==yt){DO(zn,xJ(z)[i]=xJ(x)[i] op xJ(y)[i]);} \
- else if(KJ==xt && KF==yt){DO(zn,xF(z)[i]=xJ(x)[i] op xF(y)[i]);} \
- else if(KF==xt && KJ==yt){DO(zn,xF(z)[i]=xF(x)[i] op xJ(y)[i]);} \
- else if(KF==xt && KF==yt){DO(zn,xF(z)[i]=xF(x)[i] op xF(y)[i]);} \
- else   {R E_NYI;}                                            \
+//#define DYAD_EXEC(op)  \
+///* TODO : refactor below */\
+///*a op a*/ \
+//if     (-KJ==xt && -KJ==yt){xJ(z)[0]=xJ(x)[0] op xJ(y)[0];} \
+//else if(-KJ==xt && -KF==yt){xF(z)[0]=xJ(x)[0] op xF(y)[0];} \
+//else if(-KF==xt && -KJ==yt){xF(z)[0]=xF(x)[0] op xJ(y)[0];} \
+//else if(-KF==xt && -KF==yt){xF(z)[0]=xF(x)[0] op xF(y)[0];} \
+///*a op v*/ \
+//else if(-KJ==xt && KJ==yt){DO(zn,xJ(z)[i]=xJ(x)[0] op xJ(y)[i]);} \
+//else if(-KJ==xt && KF==yt){DO(zn,xF(z)[i]=xJ(x)[0] op xF(y)[i]);} \
+//else if(-KF==xt && KJ==yt){DO(zn,xF(z)[i]=xF(x)[0] op xJ(y)[i]);} \
+//else if(-KF==xt && KF==yt){DO(zn,xF(z)[i]=xF(x)[0] op xF(y)[i]);} \
+///*v op a*/ \
+//else if(KJ==xt && -KJ==yt){DO(zn,xJ(z)[i]=xJ(x)[i] op xJ(y)[0]);} \
+//else if(KJ==xt && -KF==yt){DO(zn,xF(z)[i]=xJ(x)[i] op xF(y)[0]);} \
+//else if(KF==xt && -KJ==yt){DO(zn,xF(z)[i]=xF(x)[i] op xJ(y)[0]);} \
+//else if(KF==xt && -KF==yt){DO(zn,xF(z)[i]=xF(x)[i] op xF(y)[0]);} \
+///*v op v*/ \
+//else if(KJ==xt && KJ==yt){DO(zn,xJ(z)[i]=xJ(x)[i] op xJ(y)[i]);} \
+//else if(KJ==xt && KF==yt){DO(zn,xF(z)[i]=xJ(x)[i] op xF(y)[i]);} \
+//else if(KF==xt && KJ==yt){DO(zn,xF(z)[i]=xF(x)[i] op xJ(y)[i]);} \
+//else if(KF==xt && KF==yt){DO(zn,xF(z)[i]=xF(x)[i] op xF(y)[i]);} \
+//else   {R E_NYI;}                                            \
+//r0(x);r0(y); 
+
+#define DYAD_EXEC_OP(op, xa, ya, za) /* op, x/y/z accessor */ \
+ if     (xn==yn){DO(zn,za(z)[i]=xa(x)[i] op ya(y)[i])} \
+ else if (xn>yn){DO(zn,za(z)[i]=xa(x)[i] op ya(y)[0])} \
+ else           {DO(zn,za(z)[i]=xa(x)[0] op ya(y)[i])} \
  r0(x);r0(y);
+
+#define DYAD_EXEC_ZA(op, za)/*specify z accessor*/\
+ if     (KJ==ABS(xt) && KJ==ABS(yt)){DYAD_EXEC_OP(op,xJ,xJ,za)} \
+ else if(KJ==ABS(xt) && KF==ABS(yt)){DYAD_EXEC_OP(op,xJ,xF,za)} \
+ else if(KF==ABS(xt) && KJ==ABS(yt)){DYAD_EXEC_OP(op,xF,xJ,za)} \
+ else if(KF==ABS(xt) && KF==ABS(yt)){DYAD_EXEC_OP(op,xF,xF,za)} \
+ else   {r0(x);r0(y);R E_NYI;}
+
+#define DYAD_EXEC(op) if(KJ==ABS(zt)){DYAD_EXEC_ZA(op,xJ)}else{DYAD_EXEC_ZA(op,xF)}
 
 #endif
