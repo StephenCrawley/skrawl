3ff61579ecc70bd40aacc9ef56953efd79c8ebf7
scrawl <scrawl@email.com>
2021-11-07 01:10:46 +0000

    [~] decrements refcount of value pointed to by variable when new value is assigned to the variable

--- src/v.c
+++ src/v.c
@@ -25,7 +25,7 @@ K cat(K x,K y){K z=ABS(xt)==ABS(yt)?k(ABS(xt),xn+yn):k(KG,xn+yn);J j=0;
 K dvd(K x,K y){if(KJ==xt&&KJ==yt){y=kfj(y);};DYAD_INIT(KF);DYAD_EXEC_ZA(/,xF);R z;}
 // load+store
 K get(T *t){P(1<t->l,E_NYI);C c=*t->s;P(!('a'<=c&&'z'>=c),E_NYI);K x=vt[c-'a'];C var[10];var[0]='\'';snprintf(var+1,9,"%.*s",t->l,t->s);P(NULL==x,kerr(var));r1(x);R x;}
-K set(T *t,K x){P(1<t->l,E_NYI);C c=t->s[0];P(!('a'<=c&&'z'>=c),E_NYI);vt[c-'a']=x;r1(x);R x;}
+K set(T *t,K x){P(1<t->l,E_NYI);C c=t->s[0];P(!('a'<=c&&'z'>=c),E_NYI);if(NULL!=vt[c-'a'])r0(vt[c-'a']);vt[c-'a']=x;r1(x);R x;}
 // internal functions (for debugging)
 ZK ref(K x){K z=k(-KJ,1);xJ(z)[0]=xr-1;r0(x);R z;} // return refcount (-1 to ignore the reference of the ref function itself)
 K bng(K x,K y){P(-KJ!=xt,E_TYP);r1(y);K z= -1==xJ(x)[0]?typ(y):-2==xJ(x)[0]?ref(y):E_NYI;r0(x);r0(y);R z;}
