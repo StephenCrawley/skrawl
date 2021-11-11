3fecd2c01646889cf522b9b48c86323ddb5a7974
scrawl <scrawl@email.com>
2021-11-11 00:48:14 +0000

    [~] kerr OOB write bugfix

--- src/o.c
+++ src/o.c
@@ -10,7 +10,7 @@ K kqt(){K k=ma(1,0);xT(k)=KQ;R k;}
 K kjn(J n){K k=ma(SZ(J),n);xT(k)=KJ;R k;} K kj(){K k=kjn(1);xT(k)=-KJ;R k;}                   // return J object of length n. return J atom
 K kfn(J n){K k=ma(SZ(F),n);xT(k)=KF;R k;} K kf(){K k=kfn(1);xT(k)=-KF;R k;}                   // return F object of length n. return F atom
 K kjx(J x){K k=kj();xJ(k)[0]=x;R k;} K kfx(F x){K k=kf();xF(k)[0]=x;R k;}                     // create atom with value x
-K kerr(S e){J n=strlen(e);K x=ma(SZ(C),n+1);DO(n,xC(x)[i]=e[i]);xC(x)[n+1]='\0';xt=-128;R x;} // error object with error msg e
+K kerr(S e){J n=strlen(e);K x=ma(SZ(C),n+1);DO(n,xC(x)[i]=e[i]);xC(x)[n]='\0';xt=-128;R x;}   // error object with error msg e
 K k(I t,J n){R KK==t?kn(n):KJ==t?kjn(n):-KJ==t?kj():KF==t?kfn(n):-KF==t?kf():KN==t?knl():KQ==t?kqt():E_NYI;}
 K kxp(K x){K z=k(KK,xn);DO(xn,xK(z)[i]=KJ==ABS(xt)?kjx(xJ(x)[i]):kfx(xF(x)[i]));r0(x);R z;}   //expand. eg 1 2 3->(1;2;3) N.B. assumes types KJ/KF
 // str to K
