9c20a7c253f7737ff6a6521674bb35110f53481d
scrawl <scrawl@email.com>
2021-11-21 23:41:01 +0000

    [~] improved print of generic objects

--- src/o.c
+++ src/o.c
@@ -13,8 +13,8 @@ K kjx(J x){K k=kj();xJ(k)[0]=x;R k;} K kfx(F x){K k=kf();xF(k)[0]=x;R k;}       
 K kerr(S e){J n=strlen(e);K x=ma(SZ(C),n+1);DO(n,xC(x)[i]=e[i]);xC(x)[n]='\0';xt=-128;R x;}   // error object with error msg e
 K k(I t,J n){R KK==t?kn(n):KJ==t?kjn(n):-KJ==t?kj():KF==t?kfn(n):-KF==t?kf():KN==t?knl():KQ==t?kqt():E_NYI;}
 K kxp(K x){K z=k(KK,xn);DO(xn,xK(z)[i]=KJ==ABS(xt)?kjx(xJ(x)[i]):kfx(xF(x)[i]));r0(x);R z;}   // expand.  eg 1 2 3->(1;2;3) N.B. assumes types KJ/KF
-K sqz(K x,C s){P(KK!=xt,x);C t=xT(xK(x)[xn-1]);K z;P(0<=t&&1==xn,(z=xK(x)[0],r1(z),r0(x),z));P(0<=t,x);G b=1;DO(xn-1,b=(b&&t==xT(xK(x)[i]))); // squeeze. eg (1;2;3)->1 2 3 N.B. assumes child items are atoms
- P(!b,x);C i=0>s?-1:1;if(-KJ==t){z=k(i*KJ,xn);DO(xn,xJ(z)[i]=*xJ(xK(x)[i]));}else{z=k(i*KF,xn);DO(xn,xF(z)[i]=*xF(xK(x)[i]));};
+K sqz(K x){P(KK!=xt,x);C t=xT(xK(x)[xn-1]);K z;P(0<=t&&1==xn,(z=xK(x)[0],r1(z),r0(x),z));P(0<=t,x);G b=1;DO(xn-1,b=(b&&t==xT(xK(x)[i]))); // squeeze. eg (1;2;3)->1 2 3 N.B. assumes child items are atoms
+ P(!b,x);if(-KJ==t){z=k(KJ,xn);DO(xn,xJ(z)[i]=*xJ(xK(x)[i]));}else{z=k(KF,xn);DO(xn,xF(z)[i]=*xF(xK(x)[i]));};
  r0(x);R z;}
 // str to K
 K kjc(S s){K k=kj();*xJ(k)=strtoll(s,NULL,10);R k;} 
@@ -25,13 +25,15 @@ K kfcn(S s,I n){K k=kfn(n);S e;DO(n,xF(k)[i]=strtod(s,&e);s=e);R k;}
 V r0(K x){if(KK==xt){DO(xn,r0(xK(x)[i]))}xr--;if(1>xr)free(x);}
 K r1(K x){if(KK==xt){DO(xn,r1(xK(x)[i]))}xr++;R x;}
 
-K kfj(K x){K z=kfn(xn);DO(xn,xF(z)[i]=(F)xJ(x)[i]);r0(x);R z;} // cast. float from long
+
+// cast
+K kfj(K x){K z=kfn(xn);DO(xn,xF(z)[i]=(F)xJ(x)[i]);r0(x);R z;} // float from long
 
 // print
 ZV pj(K x){DO(xn,O("%lld",xJ(x)[i]);if(i!=xn-1)O(" "));}
 ZV pf(K x){C s[9];DO(xn,I r=snprintf(s,9,"%f.6",xF(x)[i]);if(0>r){pk(kerr("'print"));R;};G p=8;W('0'==s[p-1],--p);O("%.*s",p,s);if(i!=xn-1)O(" "))} //print F without trailing 0s
-ZV pk_(K x){if(0<xt&&1==xn)O(",");if(KK==xt){O("(");DO(xn,pk_(xK(x)[i]);if(i!=xn-1)O(";"));O(")");}else if(KJ==ABS(xt)){pj(x);}else if(KF==ABS(xt)){pf(x);}
+ZV pk_(K x){if(0<xt&&1==xn)O(",");if(KK==xt){if(1==xn){O(",");}else{O("(");}DO(xn,pk_(xK(x)[i]);if(i!=xn-1)O(";"));if(1!=xn)O(")");}else if(KJ==ABS(xt)){pj(x);}else if(KF==ABS(xt)){pf(x);}
  else if(err(x)){O("%s",xC(x));}else{pk(E_TYP);};}
 V pk(K x){if(KN==xt){r0(x);R;};if(0<xt&&1==xn)O(",");
  if(DBGP){O("typ:%d  cnt:%lld  ref:%d  dat:\n",xt,xn,xr);}; // print object meta+data
- if(KK==xt){DO(xn,pk_(xK(x)[i]);if(i!=xn-1)O("\n"))}else if(KJ==ABS(xt)){pj(x);}else if(KF==ABS(xt)){pf(x);}else if(err(x)){O("%s",xC(x));}else{pk(E_TYP);};O("\n");r0(x);}
+ if(KK==xt){if(1==xn){O(",");}DO(xn,pk_(xK(x)[i]);if(i!=xn-1)O("\n"))}else if(KJ==ABS(xt)){pj(x);}else if(KF==ABS(xt)){pf(x);}else if(err(x)){O("%s",xC(x));}else{pk(E_TYP);};O("\n");r0(x);}
