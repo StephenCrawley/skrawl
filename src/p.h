0d160a27585605a8aeca0cdeb7033eafa5a32e94
scrawl <scrawl@email.com>
2021-11-21 22:09:44 +0000

    [~] move magic number to define

--- src/p.h
+++ src/p.h
@@ -1,6 +1,8 @@
 #ifndef PARSE
 #define PARSE
 #include "a.h"
+
+#define TMAX 128 // max num of tokens that can parsed. TODO: make dynamic
 //TODO : replace this debugging with ifdef
 #define DBGT 0  // debug token
 #define DBGP 0  // debug parse
@@ -8,7 +10,7 @@
 /*            (  )  {  }  [  ]  ;  :  =  <  >  |  ?  +  -  *  %  !  &  "  @  ~  #  ,  .  '  /  \  1   2.3 "a" foo \0  not recognised */
 typedef enum {LP,RP,LB,RB,LS,RS,SC,CL,EQ,LA,RA,PI,QM,PL,HY,ST,DV,BA,AM,QT,AT,TL,HS,CM,DT,AP,FS,BS,INT,FLT,STR,ID, END,NR}TT;
 typedef struct {S s;I l;TT t;}T; // token. start,length,token type
-typedef struct {S s,c;T b[128];T *bp;}TS; // token scanner. start,current,buffer,buffer pointer
+typedef struct {S s,c;T b[TMAX];T *bp;}TS; // token scanner. start,current,buffer,buffer pointer
 TS ts;
 
 //TODO : full variable name support
