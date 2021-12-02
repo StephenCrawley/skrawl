29b2c3a7d1af31b3d1bb52b8bc677fc5cad1f8eb
scrawl <scrawl@email.com>
2021-12-02 00:43:14 +0000

    [~] cleanup

--- src/a.h
+++ src/a.h
@@ -37,7 +37,7 @@ typedef struct k{C t;I r;J n;G d[];}*K;              // type,refcount,length,dat
 #define xr    xR(x)
 #define yt    xT(y)
 #define yn    xN(y)
-// types
+// types 1 i(int),2 f(float),3 c(char)  nyi: 4 n(name),5 d(date) 
 #define KK 0  // generic
 #define KJ 1  // long   
 #define KF 2  // float
@@ -55,68 +55,3 @@ typedef struct k{C t;I r;J n;G d[];}*K;              // type,refcount,length,dat
 #define E_RNK   kerr("'rnk")
 
 #endif
-
-/*
-ktypes 1 i(int),2 f(float),3 c(char),4 n(name),5 d(date) 
-
-
-inspiration for above
-
-kx k.h(partial) - typedefs and K object
-
-typedef char*S,C;typedef unsigned char G;typedef short H;typedef int I;typedef long long J;typedef float E;typedef double F;typedef void V;
-
-typedef struct k0 {
-    signed char m,a,t;  // m,a are for internal use.
-    C u;
-    I r; 
-    union {
-        G  g; 
-        H  h; 
-        I  i; 
-        J  j; 
-        E  e; 
-        F  f; 
-        S  s;
-
-        struct k0*k;
-        struct {J  n;G  G0 [1];};
-    }; 
-} *K;
-
-shakti k.h
-
-#include<string.h>
-typedef unsigned I;typedef long J;typedef double F;typedef unsigned char G,*S;typedef unsigned long U,K;
-enum{KC=1,KS=2,KI=13,KJ,KT=21,KD=25,KF=43,XT=253,XD};
-
-#define N(n,a...)   {int _=(n),i=0;while(i<_){a;++i;}}
-#define S(i,c,a...) switch(i){c default:{a;}}
-#define P(b,a...)   if(b)return({a;});
-#define C(i,a...)   case i:{a;}break;
-#define R           return
-
-//atom: type value
-#define tx (x>>56)
-#define xi ((I)x)
-#define xj *(J*)(0xffffffffffff&x)
-#define xf *(F*)(0xffffffffffff&x)
-
-//list: type count values
-#define xt ((S)x)[-5]
-#define yt ((S)y)[-5]
-#define xn ((int*)x)[-1]
-#define yn ((int*)y)[-1]
-#define fn ((int*)f)[-1]
-#define xI ((I*)x)
-#define xJ ((J*)x)
-#define xF ((F*)x)
-#define xK ((K*)x)
-#define yK ((K*)y)
-
-K k(K,...);
-
-K ki(I i){R k('i',i);}           K D(K x,K y){R k('a',x,y);}    
-K kf(F f){R k('f',*(K*)&f);}     K T(K x,K y){R k('A',x,y);}
-K kC(S s){I n=strlen((char*)s);R(K)memcpy((S)k('C',n),s,n);}
-*/
