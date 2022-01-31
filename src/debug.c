283accf9e13d766c8b1c44c0a462285c5d54b16d
scrawl <scrawl@email.com>
2022-01-31 00:48:31 +0000

    [~] bigfix memleak. temp variable wasn't deleted when disassembling bytecode

--- src/debug.c
+++ src/debug.c
@@ -3,8 +3,14 @@
 #include "object.h"
 #include "vm.h"
 
+// this function prints 1 line per instruction
+// prints opcode, opcode name, stack, eg:
+// 032  OP_ENLIST      (3 4;1 2)
 void disassemble(VM *vm, uint8_t instruction){
+    // print opcode
     printf("%03d  ", instruction);
+
+    // print opcode name
     switch(instruction){
         case OP_CONSTANT : {
             printf("%-15s", "OP_CONSTANT");
@@ -26,8 +32,11 @@ void disassemble(VM *vm, uint8_t instruction){
         default : printf("%-15s", "OP_UNKNOWN");
     }
 
+    // print stack
     K t = k(KK, (uint64_t)(vm->top - vm->stack));
     for (uint64_t i = 0; i < tn; ++i) tk[i] = vm->stack[i];
     printOneLineK(t);
+    free(t);
+
     putchar('\n');
 }
