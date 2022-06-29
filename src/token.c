3b701f9e1f4ad02a4d9b6244ead169b8c09217da
scrawl <scrawl@email.com>
2022-06-30 00:58:24 +0100

    [~] bugfix - tokenization

--- src/token.c
+++ src/token.c
@@ -89,12 +89,15 @@ static Token dotToken(Scanner *scanner){
 // 2) - 3+1 (parsed as -(3+1), or "negate 3+1". evaluates to -4)
 static Token minusToken(Scanner *scanner){
     if (isNumber(*scanner->current)){
-        if (' ' == scanner->start[-1]      || 
+        if (scanner->source == scanner->start){
+            return numberToken(scanner, 0);
+        }
+        else if (
+            ' ' == scanner->start[-1]      || 
             '(' == scanner->start[-1]      || 
             '[' == scanner->start[-1]      || 
             '{' == scanner->start[-1]      ||
-            isOperator(scanner->start[-1]) ||
-            scanner->source == scanner->start){ 
+            isOperator(scanner->start[-1]) ){ 
                 return numberToken(scanner, 0);
         }
     }
