9f16a20e908747084183b1e5f196ccf279b21329
scrawl <scrawl@email.com>
2022-11-24 01:10:35 +0000

    negative numbers now handled

--- src/parse.c
+++ src/parse.c
@@ -1,7 +1,7 @@
 #include "skrawl.h"
 #include "parse.h"
 
-#define EXPR_END() sc(";\n\0", *parser->current)
+#define AT_EXPR_END() sc(";\n\0", *parser->current)
 
 // foward declarations
 static void Exprs(Parser *parser);
@@ -17,10 +17,14 @@ static char class(char c){
 
 // parse number(s). eg "1" or "1 23"
 static void parseNum(Parser *parser){
+    char c;
     do {
+        if ('-'==*parser->current) ++parser->current;
         while ('0'==class(*parser->current)) ++parser->current;
+        if(' '!=*parser->current) return;
         ws(parser);
-    } while ('0'==class(*parser->current));
+        c = *parser->current;
+    } while ('0'==class(c) || ('-'==c&&'0'==class(parser->current[1])));
 }
 
 // parse single expression
@@ -28,15 +32,18 @@ static void expr(Parser *parser){
     ws(parser);
     char a = *parser->current++;
     char c = class(a);
+    // if '-' followed by num, we're parsing a num, so reclassify
+    if ('-'==a && '0'==class(*parser->current)) c = '0';
 
     switch (c){
-    case '0': parseNum(parser); break;
+    case '0': --parser->current, parseNum(parser); break;
     case '(': Exprs(parser); if(')'==(a=*parser->current++)){ break; } //else fallthrough
-    default : printf("'error: %c\n", a);
+    default : printf("'parse: %c\n", a); return;
     }
 
     ws(parser);
-    if (EXPR_END()) return;
+    if (AT_EXPR_END()) return;
+    printf("'parse: expected expr end");
 }
 
 // parse ;-delimited Expressions
