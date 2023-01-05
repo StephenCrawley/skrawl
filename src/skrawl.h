ed1a608664b9d68e62a3748a7cdd550323397bb2
scrawl <scrawl@email.com>
2023-01-05 14:29:36 +0000

    [+] completed VERB_STR so all verbs included, removed unnecessary assignments from skrawl.h macros

--- src/skrawl.h
+++ src/skrawl.h
@@ -39,7 +39,7 @@ enum {
     K_ADVERB_END = KEL         //end of adverb types
 };
 
-#define VERB_STR    ":+-*%,;"
+#define VERB_STR    ":+-*%,?.@!$#_^&?=<>~|;"
 #define ADVERB_STR  "'/\\'/\\"
 
 // K header accessors
@@ -61,8 +61,8 @@ enum {
 #define ABS(a)   __extension__({__typeof__(a)_a=(a); _a > 0 ? _a : -_a ;}) 
 #define MAX(a,b) __extension__({__typeof__(a)_a=(a);__typeof__(b)_b=(b);_a>_b?_a:_b;})
 #define IS_SIMPLE_LIST(x)  __extension__({__typeof__(x)_x=(x); 0<TYP(_x) && TYP(_x)<=K_SIMPLE_LIST_END;})
-#define IS_VERB(a)    __extension__({__typeof__(a)_a=(a); i8 t=TYP(_a); KU==t || KV==t;}) 
-#define IS_ADVERB(a)  __extension__({__typeof__(a)_a=(a); i8 t=TYP(_a); K_ADVERB_START<=t && t<=K_ADVERB_END;})
+#define IS_VERB(a)    __extension__({i8 t=TYP(a); KU==t || KV==t;}) 
+#define IS_ADVERB(a)  __extension__({i8 t=TYP(a); K_ADVERB_START<=t && t<=K_ADVERB_END;})
 #define IS_GENERIC(x) __extension__({__typeof__(x)_x=(x); i8 t=TYP(_x); KK==t || KL==t || IS_ADVERB(_x);}) //has other K objects as children
 // shared utility functions
 static inline char* sc(char *s,char c){ while(*s!=c)if(!*s++)return (char*)0; return s; }
