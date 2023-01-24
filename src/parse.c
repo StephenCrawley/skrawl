e9300998e98730f2fb58f7ce16844d61ed905317
scrawl <scrawl@email.com>
2023-01-24 22:16:10 +0000

    [~] rewrite next() in terms of peek() in parse.c

--- src/parse.c
+++ src/parse.c
@@ -16,11 +16,11 @@ static inline Parser *dec(Parser *p){ return --p->current, p; }
 // consume whitepace
 static inline void ws(Parser *p){ while(' '==*p->current) ++p->current; }
 
-// next char. consume whitespace and consume & return next char
-static inline char next(Parser *p){ ws(p); char a=*p->current++; return '/'==a&&' '==p->current[-2] ? 0 : a; }
-
 // peek char. consume whitespace and peek next char (return but don't consume it)
 static inline char peek(Parser *p){ ws(p); char a=*p->current; return '/'==a&&' '==p->current[-1] ? 0 : a; }
+
+// next char. consume whitespace and consume & return next char
+static inline char next(Parser *p){ char a=peek(p); return ++p->current, a; }
 
 // return char class 
 static char class(char c){
