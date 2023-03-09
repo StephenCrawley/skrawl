8341b97677077a896dbcb81219d2fbc3976c9365
scrawl <scrawl@email.com>
2023-03-09 19:11:23 +0000

    [~] add guard to mapleft() to error on atom and dict

--- src/adverb.c
+++ src/adverb.c
@@ -4,6 +4,11 @@
 // x f\:y
 // where f is a function pointer
 K mapleft(DYAD f, K x, K y){
+    // can only map (int-indexable) lists
+    if (IS_ATOM(x) || TYP(x)==KD)
+        return UNREF_XY(kerr("'rank!"));
+
+    // map over x
     K r=tn(KK,0),t;
     for (i64 i=0,n=KCOUNT(x); i<n; i++){
         t=f(item(i,x),ref(y));
