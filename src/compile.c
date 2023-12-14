4c7b8e56abae640141375ee83513f28369684242
scrawl <scrawl@email.com>
2023-12-14 01:09:13 +0000

    [+] lambdas can now access global vars

--- src/compile.c
+++ src/compile.c
@@ -6,7 +6,7 @@
 #define BYTES(x)     LAMBDA_OPCODE(x) //bytecodes accessor
 #define CONSTS(x)    LAMBDA_CONSTS(x) //constants accessor
 
-#define COMPILING_LAMBDA(x) (HDR_CNT(x) != 2)
+#define IS_LAMBDA(x) (HDR_CNT(x) != 2)
 
 // in this script x is the parse tree (or any child object within it)
 // and r is the object to be returned to the VM (see compile() below)
@@ -49,16 +49,10 @@ static K compileApplyN(K r, i64 n){
 
 static K compileVarGet(K x, K r){
     // global get
-    if (!COMPILING_LAMBDA(r))
+    i64 ix;
+    if (!IS_LAMBDA(r) || (ix=symIndex(x,LAMBDA_ARGS(r))) == CNT(LAMBDA_ARGS(r)))
         return addByte(compileConstant(r,x),OP_GET_GLOBAL);
-
-    // if compiling lambda, either we're getting an arg or a local
-    K args=LAMBDA_ARGS(r);
-    i64 ix=symIndex(x,args);
-    if (ix < HDR_CNT(args))
-        return addByte(r,OP_GET_ARG+ix);
-    else 
-        return UNREF_R(kerr("'compile! lambda vars nyi"));
+    return addByte(r,OP_GET_ARG+ix);
 }
 
 static K compileExprs(K x, K r){
@@ -103,30 +97,13 @@ static K compileExprs(K x, K r){
         compileApplyN(r,xn-1);                                      //general apply
 }
 
-static K compile0(K x, K r){
-    // if compiling single expression i.e. not(;:;...)
-    if (TYP(x)!=KK || !CNT(x) || TYP(*OBJ(x))!=KU || TAG_VAL(*OBJ(x))!=TOK_SEMICOLON){
-        r=compileExprs(x,r);
-        return UNREF_X(IS_ERROR(r) ? r : addByte(r,OP_RETURN));
-    }
-    
-    // else compile each expression from 1 .. n-1
-    for (i64 i=1,n=CNT(x)-1; i<=n; i++){
-        r=compileExprs(OBJ(x)[i],r);
-        if (IS_ERROR(r)) return r;
-        // append OP_RETURN if last expression, else append OP_POP
-        r=addByte(r, i==n ? OP_RETURN : OP_POP);
-    }
-    return UNREF_X(r);
-}
-
 // given a pointer to bytecode, determine the stack size
 static u8 getMaxStackSize(u8 *b){
     u8 s=0,m=0;
     while (*b != OP_RETURN){
         u8 instr=*b++;
-        switch (20u > (u32)(instr-OP_MONAD  ) ? OP_MONAD   :
-                20u > (u32)(instr-OP_DYAD   ) ? OP_DYAD    :
+        switch (32u > (u32)(instr-OP_MONAD  ) ? OP_MONAD   :
+                32u > (u32)(instr-OP_DYAD   ) ? OP_DYAD    :
                  6u > (u32)(instr-OP_ADVERB ) ? OP_ADVERB  : 
                  3u > (u32)(instr-OP_GET_ARG) ? OP_GET_ARG : instr){
         case OP_CONSTANT:
@@ -153,20 +130,39 @@ static u8 getMaxStackSize(u8 *b){
     return m;
 }
 
+static K compile0(K x, K r){
+
+    // if compiling multiple Exprs, compile each expression from 1 .. n-1
+    if (CNT(x) && TYP(x)==KK && IS_MONAD(*OBJ(x),TOK_SEMICOLON)){
+        for (i64 i=1,n=CNT(x)-1; i<=n; i++){
+            r=compileExprs(OBJ(x)[i],r);
+            if (IS_ERROR(r)) return r;
+            // append OP_RETURN if last expression, else append OP_POP
+            if (i != n) r=addByte(r,OP_POP);
+        }
+    }
+    // else compile single expression
+    else {
+        r=compileExprs(x,r);
+        if (IS_ERROR(r)) return r;
+    }
+
+    r=addByte(r,OP_RETURN);
+    ((u8*)BYTES(r))[0]=getMaxStackSize((u8*)BYTES(r)+1);
+    return UNREF_X(r);
+}
+
 // recurse thru parse tree x, generating bytecode
 // returns a K object (byetcode;consts)
 K compile(K x){ 
-    x=compile0(x, k2(tn(KX,1),tn(KK,0)));
-    ((u8*)BYTES(x))[0]=getMaxStackSize((u8*)BYTES(x)+1);
-    return x;
+    return compile0(x, k2(tn(KX,1),tn(KK,0)));
 }
 
 // x - parse tree
 // f - ("{x}";args) of lambda
 K compileLambda(K x, K f){
-    K r=j2(k2(tn(KX,1),tn(KK,0)),f); //(opcodes;consts;"{x}";args)
-    r=compile0(x,r);
-    ((u8*)BYTES(r))[0]=getMaxStackSize((u8*)BYTES(r)+1);
-    HDR_RNK(r)=HDR_CNT(LAMBDA_ARGS(r));
-    return r;
+    x=compile0(x, j2(k2(tn(KX,1),tn(KK,0)),f));
+    if (IS_ERROR(x)) return x;
+    HDR_RNK(x)=HDR_CNT(LAMBDA_ARGS(x));
+    return x;
 }
